a5e180e2d6dce885abe63026c98a8eb4eddc4dab
sivasrinivas <sivasrinivas.amara@gmail.com>
2018-07-21 09:32:23 -0400

    running ng build

--- client/dist/client/main.js
+++ client/dist/client/main.js
@@ -41,7 +41,7 @@ module.exports = ""
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<!doctype html>\n<html lang=\"en\">\n<head>\n  <!-- Required meta tags -->\n  <meta charset=\"utf-8\">\n  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">\n\n  <!-- Bootstrap CSS -->\n  <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css\" integrity=\"sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB\" crossorigin=\"anonymous\">\n\n  <title>Random Facts</title>\n</head>\n<body>\n<nav class=\"navbar navbar-expand-md navbar-dark bg-dark fixed-top\">\n  <a class=\"navbar-brand\" href=\"#\">Random Facts</a>\n  <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarsExampleDefault\" aria-controls=\"navbarsExampleDefault\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n    <span class=\"navbar-toggler-icon\"></span>\n  </button>\n</nav>\n<br><br><br><br>\n<main role=\"main\" class=\"container\">\n  <app-facts></app-facts>\n</main><!-- /.container -->\n\n<!-- Optional JavaScript -->\n<!-- jQuery first, then Popper.js, then Bootstrap JS -->\n<script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\"></script>\n<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js\" integrity=\"sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49\" crossorigin=\"anonymous\"></script>\n<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js\" integrity=\"sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T\" crossorigin=\"anonymous\"></script>\n</body>\n</html>\n"
+module.exports = "<!doctype html>\n<html lang=\"en\">\n<head>\n  <!-- Required meta tags -->\n  <meta charset=\"utf-8\">\n  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">\n\n  <!-- Bootstrap CSS -->\n  <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css\" integrity=\"sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB\" crossorigin=\"anonymous\">\n\n  <!-- FontAwesome Icons-->\n  <link href=\"https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css\" rel=\"stylesheet\">\n\n  <title>Random Facts</title>\n</head>\n<body>\n<nav class=\"navbar navbar-expand-md navbar-dark bg-dark fixed-top\">\n  <a class=\"navbar-brand\" href=\"#\">Random Facts</a>\n  <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarsExampleDefault\" aria-controls=\"navbarsExampleDefault\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n    <span class=\"navbar-toggler-icon\"></span>\n  </button>\n</nav>\n<br><br><br><br>\n<main role=\"main\" class=\"container\">\n  <app-facts></app-facts>\n</main><!-- /.container -->\n\n<!-- Optional JavaScript -->\n<!-- jQuery first, then Popper.js, then Bootstrap JS -->\n<script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\"></script>\n<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js\" integrity=\"sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49\" crossorigin=\"anonymous\"></script>\n<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js\" integrity=\"sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T\" crossorigin=\"anonymous\"></script>\n</body>\n</html>\n"
 
 /***/ }),
 
@@ -96,14 +96,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm5/forms.js");
 /* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
 /* harmony import */ var _facts_facts_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./facts/facts.component */ "./src/app/facts/facts.component.ts");
-/* harmony import */ var _fact_detail_fact_detail_component__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./fact-detail/fact-detail.component */ "./src/app/fact-detail/fact-detail.component.ts");
 var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-
 
 
 
@@ -117,7 +115,6 @@ var AppModule = /** @class */ (function () {
             declarations: [
                 _app_component__WEBPACK_IMPORTED_MODULE_3__["AppComponent"],
                 _facts_facts_component__WEBPACK_IMPORTED_MODULE_4__["FactsComponent"],
-                _fact_detail_fact_detail_component__WEBPACK_IMPORTED_MODULE_5__["FactDetailComponent"]
             ],
             imports: [
                 _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"],
@@ -128,69 +125,6 @@ var AppModule = /** @class */ (function () {
         })
     ], AppModule);
     return AppModule;
-}());
-
-
-
-/***/ }),
-
-/***/ "./src/app/fact-detail/fact-detail.component.css":
-/*!*******************************************************!*\
-  !*** ./src/app/fact-detail/fact-detail.component.css ***!
-  \*******************************************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-module.exports = ""
-
-/***/ }),
-
-/***/ "./src/app/fact-detail/fact-detail.component.html":
-/*!********************************************************!*\
-  !*** ./src/app/fact-detail/fact-detail.component.html ***!
-  \********************************************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-module.exports = "<p>\n  fact-detail works!\n</p>\n"
-
-/***/ }),
-
-/***/ "./src/app/fact-detail/fact-detail.component.ts":
-/*!******************************************************!*\
-  !*** ./src/app/fact-detail/fact-detail.component.ts ***!
-  \******************************************************/
-/*! exports provided: FactDetailComponent */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "FactDetailComponent", function() { return FactDetailComponent; });
-/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
-var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (undefined && undefined.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-
-var FactDetailComponent = /** @class */ (function () {
-    function FactDetailComponent() {
-    }
-    FactDetailComponent.prototype.ngOnInit = function () {
-    };
-    FactDetailComponent = __decorate([
-        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
-            selector: 'app-fact-detail',
-            template: __webpack_require__(/*! ./fact-detail.component.html */ "./src/app/fact-detail/fact-detail.component.html"),
-            styles: [__webpack_require__(/*! ./fact-detail.component.css */ "./src/app/fact-detail/fact-detail.component.css")]
-        }),
-        __metadata("design:paramtypes", [])
-    ], FactDetailComponent);
-    return FactDetailComponent;
 }());
 
 
@@ -263,7 +197,7 @@ module.exports = ""
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<div *ngFor=\"let fact of facts\">\n  <div class=\"col-8 align-self-center\">\n    <div class=\"card mb-4 box-shadow\">\n      <div class=\"card-body\">\n        <p>{{fact.fact}}</p>\n      </div>\n    </div>\n  </div>\n</div>\n"
+module.exports = "<div *ngFor=\"let fact of facts\">\n  <div class=\"col-8 offset-2 align-self-center\">\n    <div class=\"card mb-4 box-shadow\">\n      <div class=\"card-body\">\n        <div class=\"float-left card-text\">{{fact.fact}}</div>\n        <div class=\"float-right btn-group\">\n          <button class=\"btn btn-sm btn-outline-secondary\" type=\"button\">\n            <i class=\"fa fa-heart\"></i>\n          </button>\n          <button class=\"btn btn-sm btn-outline-secondary\" type=\"button\" (click)='incLike(fact)'>\n            <i class=\"fa fa-thumbs-up\"></i> {{fact.likes}}\n          </button>\n        </div>\n      </div>\n    </div>\n  </div>\n</div>\n"
 
 /***/ }),
 
@@ -302,6 +236,9 @@ var FactsComponent = /** @class */ (function () {
         this.factService.getFacts()
             .subscribe(function (facts) { return _this.facts = facts; });
     };
+    FactsComponent.prototype.incLike = function (fact) {
+        fact.likes++;
+    };
     FactsComponent = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
             selector: 'app-facts',
@@ -328,14 +265,14 @@ var FactsComponent = /** @class */ (function () {
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "FACTS", function() { return FACTS; });
 var FACTS = [
-    { id: 1, fact: 'India has a Bill of Rights for cows.' },
-    { id: 2, fact: 'Contrary to popular belief, opossums, squirrels, chipmunks, and mice do not carry rabies.' },
-    { id: 3, fact: 'Los Angeles\'s full name is \\"El Pueblo de Nuestra Senora la Reina de los Angeles de Porciuncula\\"!' },
-    { id: 4, fact: 'In 2003, the U.S. Government spent about $2,000,000.00 on potato research!' },
-    { id: 5, fact: 'It takes 492 seconds for sunlight to reach the Earth!' },
-    { id: 6, fact: 'The electric chair was invented by a dentist!' },
-    { id: 7, fact: 'A polar bear\'s skin is black. Its fur is not white, but actually clear.' },
-    { id: 8, fact: 'The first telephone book was one page long and had only 50 names in it.' }
+    { id: 1, fact: 'India has a Bill of Rights for cows.', likes: 1 },
+    { id: 2, fact: 'Contrary to popular belief, opossums, squirrels, chipmunks, and mice do not carry rabies.', likes: 3 },
+    { id: 3, fact: 'Los Angeles\'s full name is \\"El Pueblo de Nuestra Senora la Reina de los Angeles de Porciuncula\\"!', likes: 2 },
+    { id: 4, fact: 'In 2003, the U.S. Government spent about $2,000,000.00 on potato research!', likes: 1 },
+    { id: 5, fact: 'It takes 492 seconds for sunlight to reach the Earth!', likes: 4 },
+    { id: 6, fact: 'The electric chair was invented by a dentist!', likes: 1 },
+    { id: 7, fact: 'A polar bear\'s skin is black. Its fur is not white, but actually clear.', likes: 4 },
+    { id: 8, fact: 'The first telephone book was one page long and had only 50 names in it.', likes: 1 }
 ];
 
 
